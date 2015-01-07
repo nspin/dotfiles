c29addad67fdb6fa12eba14dff5e1c41bb09e113
nick <you@example.com>
2015-01-07 16:14:34 -0600

    improved furnish script

--- furnish.sh
+++ furnish.sh
@@ -9,19 +9,19 @@ dir=~/dotfiles                  # dotfiles dir
 olddir=~/dotfiles_old           # old dotfiles backup dir
 files="vimrc vim screenrc"        # files/dirs to symlink in ~
 
-echo -n "Creating $olddir..."
+echo -n "Creating $olddir ..."
 mkdir -p $olddir
 echo "done"
 
-echo -n "Changing to $dir..."
+echo -n "Changing to $dir ..."
 cd $dir
 echo "done"
 
 for file in $files; do
-    echo -n "Moving existing symlink to $file from ~ to $olddir..."
+    echo -n "Moving existing $file from ~ to $olddir ..."
     mv ~/.$file ~/dotfiles_old/
     echo "done"
-    echo -n "Creating symlink to $file in home directory..."
+    echo -n "Creating symlink to $file in ~ ..."
     ln -s $dir/$file ~/.$file
     echo "done"
 done
