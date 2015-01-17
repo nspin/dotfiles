35e6d8798c79b4a0296f382273189c7aa9f1918a
Nick Spinale <spinalen@carleton.edu>
2015-01-16 18:55:04 -0600

    Update xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,5 @@ import XMonad
 
 main = xmonad $ defaultConfig
     { normalBorderColor  = "#031518"
-    , focusedBorderColor = "#031518" --"#b58900"
-    , focusFollowsMouse  = False
+    , focusedBorderColor = "#b58900"
     }
