39605c67d13c41d1e5e10e873391d8f4d52d5266
nickspinale <you@example.com>
2015-03-25 06:43:49 -0500

    new bash scripts for installing config

--- infect.sh
+++ infect.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 ############################
-# Makes all necessary symlinks to files/dirs in ~/dotfiles,
-# and moves old ones to ~/dotfiles_old
-# (note space at end of min)
+# Makes all necessary symlinks in ~
+# and moves old ones to $OLD
 ############################
 
-dir=~/dotfiles/core             # dotfiles dir
-olddir=~/dotfiles_old           # old dotfiles backup dir
-min="vimrc vim bashrc bash_profile gitconfig gitignore_global screenrc "
-rest="tmux.conf xinitrc Xresources xmonad"
+OLD=~/dotfiles_old           # old dotfiles backup dir
 
-files=$min
+read -p "# INPUT \"c\" FOR CORE OR \"a\" FOR All: " $CHOICE
 
-read -p "All (y/n)? " all
-
-if [ $all == "y" ] ; then
-    files=$files$rest
+if [ $CHOICE == "c" ] ; then
+    $DIRS="core"
+elif [ $CHOICE == "a" ] ; then
+    $DIRS="core extras"
+else
+    $DIRS=""
 fi
 
-echo -n "Creating $olddir ..."
-mkdir -p $olddir
-echo "done"
+echo -n "# CREATING $OLD"
+mkdir -p $OLD
+echo "DONE"
 
-echo -n "Changing to $dir ..."
-cd $dir
-echo "done"
-
-for file in $files; do
-    echo -n "Moving existing $file from ~ to $olddir ..."
-    mv ~/.$file $olddir
-    echo "done"
-    echo -n "Creating symlink to $file in ~ ..."
-    ln -s $dir/$file ~/.$file
-    echo "done"
+for DIR in $DIRS ; do
+    echo -n "# ADDING FILES IN $DIR ..."
+    cd $DIR
+    for FILE in ./* ; do
+        mv ~/$FILE $OLD
+        ln -s $FILE ~
+    done
+    cd ..
+    echo "DONE"
 done
+
+echo -n "# COMPLETE"
