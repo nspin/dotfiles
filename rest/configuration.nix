b468755909b08d5be478ce5483dab9c9c58e5e51
Nick Spinale <spinalen@carleton.edu>
2015-07-22 20:24:23 +0200

    recovery

--- rest/configuration.nix
+++ rest/configuration.nix
@@ -26,6 +26,7 @@
 
     manpages
 
+    xterm
     tmux
     screen
     vimHugeX
@@ -57,28 +58,34 @@
   services.xserver.enable = true;
   services.xserver.layout = "us";
 
-  services.xserver.displayManager.session =
-    [ { manage = "window";
-        name = "xmonad";
-        start = ''
-            xset r rate 300 50
-            ${pkgs.haskellPackages.xmonad}/bin/xmonad &
-        '';
-      }
-      { manage = "desktop";
-        name = "xterm";
-        start = ''
-            ${pkgs.xterm}/bin/xterm -ls &
-        '';
-      }
-    ];
+  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+
+# services.xserver.displayManager.session =
+#   [ { manage = "window";
+#       name = "xmonad";
+#       start = ''
+#           xset r rate 300 50
+#           ${pkgs.haskellPackages.xmonad}/bin/xmonad &
+#       '';
+#     }
+#     { manage = "desktop";
+#       name = "xterm";
+#       start = ''
+#           ${pkgs.xterm}/bin/xterm -ls &
+#       '';
+#     }
+#   ];
 
   services.xserver.displayManager.slim.enable = true;
+  services.xserver.displayManager.slim.defaultUser = "nick";
   # services.xserver.displayManager.slim.theme = pkgs.slimThemes.nixosSlim;
   services.xserver.displayManager.slim.theme = pkgs.fetchurl {
       url = "https://github.com/jagajaga/nixos-slim-theme/archive/Final.tar.gz";
       sha256 = "4cab5987a7f1ad3cc463780d9f1ee3fbf43603105e6a6e538e4c2147bde3ee6b";
     };
+
+  services.xserver.desktopManager.xterm.enable = true;
 
   users.extraUsers.nick = {
     isNormalUser = true;
