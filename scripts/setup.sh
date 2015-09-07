7f6869b8ce0998052af63b60e37f7be7c860c2a6
Nick Spinale <spinalen@carleton.edu>
2015-09-07 19:04:44 +0000

    re-added vundle submodule

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@ while read line; do
     read a b <<< $(echo $line | awk '{ print $1; print $2; }')
     mkdir -p $(dirname $b)
     ln -sf $basedir/src/$a $b
-done < "$basedir/scripts/links.txt
+done < "$basedir/scripts/links.txt"
