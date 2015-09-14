902fd9aec2bfb0bb621eb985872e255a366c75fa
Nick Spinale <spinalen@carleton.edu>
2015-09-14 16:02:13 -0500

    changed setup script for mac compatibility

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@ basedir="$HOME/dotfiles"
 while read line; do
     read a b <<< $(echo $line | awk '{ print $1; print $2; }')
     mkdir -p $(dirname $b)
-    ln -sfT $basedir/src/$a $b
+    ln -sfnv $basedir/src/$a $b
 done
