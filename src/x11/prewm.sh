516c968be8ad755dd1f76593f22d9701a28ec5ee
Nick Spinale <spinalen@carleton.edu>
2015-09-15 16:03:02 -0500

    modularizing xresources

--- src/x11/prewm.sh
+++ src/x11/prewm.sh
@@ -1,4 +1,5 @@
-xmodmap /home/nick/dotfiles/src/x11/Xmodmap
-xrdb -merge /home/nick/dotfiles/src/x11/Xresources
+xmodmap $HOME/dotfiles/src/x11/Xmodmap
+xrdb -merge $HOME/dotfiles/src/x11/Xresources.xterm
+xrdb -merge $HOME/dotfiles/src/x11/xresources/Xresources.dark
 xset r rate 300 50
 xsetroot -solid '#002B36'
