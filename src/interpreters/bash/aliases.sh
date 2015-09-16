06d823ab8ede2f784d24f188ebb77a9e110b8a10
Nick Spinale <spinalen@carleton.edu>
2015-09-15 21:46:35 -0500

    organizing bash stuff

---
+++ src/interpreters/bash/aliases.sh
@@ -0,0 +1,37 @@
+#--------------------
+# ALIASES
+#--------------------
+
+# general
+# alias d='cd'
+# alias p='cd ..'
+# alias r='cd /'
+
+# screen
+alias sc='screen -dRR'
+
+# tmux
+alias tm='tmux'
+alias ta='tmux attach'
+alias tt='tmux attach -t'
+alias tl='tmux ls'
+
+# vim colors inside of putty
+alias ptm='TERM=screen-256color-bce tmux -u'
+alias pta='TERM=screen-256color-bce tmux attach -u'
+alias ptt='TERM=screen-256color-bce tmux attach -u -t'
+
+# git
+alias ga='git add -A :/'
+alias gs='git status'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git pull'
+alias gm='git submodule update --init'
+alias gu='git submodule foreach git pull origin master'
+
+# cabal
+alias cr='cabal repl'
+alias ci='cabal sandbox init'
+alias ca='cabal sandbox add-source'
+alias co='cabal install --dependencies-only'
--- src/nix-config/config.nix
+++ src/nix-config/config.nix
@@ -1,4 +1,13 @@
 {
   packageOverrides = pkgs: rec {
+#    fzf-tmux = pkgs.stdenv.mkDerivation {
+#      name = "fzf-tmux";
+#      buildInputs = [ pkgs.fzf ]
+#      builder = ''
+#          mkdir -p "$out/bin"
+#
+#        '';
+#      src = /home/nick/dotfiles/src/interpreters/bash/fzf/bin;
+#    }
   };
-}
+};
