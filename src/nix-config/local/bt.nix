9305cfb5401c2fa57bad38fb5f8e15f4b7f87a17
Nick Spinale <spinalen@carleton.edu>
2015-11-03 21:02:55 -0600

    updated nix expr for bt stuff

--- src/nix-config/local/bt.nix
+++ src/nix-config/local/bt.nix
@@ -1,12 +1,16 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 
+with import /home/nick/.nix-defexpr/channels_root/nixos/pkgs/development/haskell-modules/lib.nix { pkgs = nixpkgs; };
+
 (if compiler == "default"
      then nixpkgs.pkgs.haskellPackages
      else nixpkgs.pkgs.haskell.packages.${compiler}).override {
   overrides = self: super: {
       mod-n = self.callPackage ./mod-n {};
-      bigword = self.callPackage ./bigword {};
+      bigword = dontCheck (self.callPackage ./bigword {});
+      # bigword = self.callPackage ./bigword {};
       bencode = self.callPackage ./bencode {};
       aether = self.callPackage ./aether {};
+      data-type-util = self.callPackage ./data-type-util {};
     };
 }
