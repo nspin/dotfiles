73b658cfd8960b20e58c1b30d12a0064fbbcbdc0
Nick Spinale <spinalen@carleton.edu>
2015-12-08 12:35:08 -0600

    small update for datrium

---
+++ src/nix-config/datrium.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  services.vmwareGuest.enable = true;
+
+  environment.systemPackages = with pkgs; [
+
+    adobe-reader
+
+  # nix
+    nix-repl
+    nix-prefetch-scripts
+
+  # misc command line utils
+    zip
+    unzip
+    tree
+    fzf
+
+  # random
+    rlwrap
+
+  # x tools
+    xclip
+    xorg.xkill
+    xlibs.xmodmap
+
+  # x utils
+    dmenu
+    gmrun
+    xlibs.xmessage
+    haskell.packages.ghc7102.xmobar
+
+  # fonts
+    dejavu_fonts
+
+  # x applications
+    firefox
+    xchat
+
+  # general development
+    tmux
+    vimHugeX
+    git
+
+  # other languages
+    gcc
+    python
+  ];
+}
