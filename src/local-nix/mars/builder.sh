4c9fedd09b21060c2d2ace103eef4f527ed9136c
Nick Spinale <spinalen@carleton.edu>
2016-01-13 18:30:54 -0600

    fixed mars executable

--- src/local-nix/mars/builder.sh
+++ src/local-nix/mars/builder.sh
@@ -6,5 +6,5 @@ mkdir -p $out/share/java
 cp $src $out/share/java/Mars4_5.jar
 
 echo '#!/bin/sh' > $out/bin/mars
-echo "java -jar $out/share/java/Mars4_5.jar" '$@' >> $out/bin/mars
+echo "$jdk/bin/java -jar $out/share/java/Mars4_5.jar" '$@' >> $out/bin/mars
 chmod +x $out/bin/mars
