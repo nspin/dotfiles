b97fa9a649f1f561403f2566f0b1ad50bf184b54
Nick Spinale <spinalen@carleton.edu>
2016-01-31 14:55:49 -0600

    fixed fzf plugin

--- src/nix-config/nixpkgs-config/local-pkgs/fzf-vim/builder.sh
+++ src/nix-config/nixpkgs-config/local-pkgs/fzf-vim/builder.sh
@@ -2,9 +2,9 @@ source $stdenv/setup
 cp --no-preserve=mode $src fzf.vim
 
 cp --no-preserve=mode $aPatch fzf.vim.patch
-echo "> let s:fzf_go = '$fzf'" >> fzf.vim.patch
+echo "> let s:fzf_go = '$fzf/bin/fzf'" >> fzf.vim.patch
 cat $bPatch >> fzf.vim.patch
-echo "> let s:fzf_tmux = '$fzfTmux'" >> fzf.vim.patch
+echo "> let s:fzf_tmux = '$fzfTmux/bin/fzf-tmux'" >> fzf.vim.patch
 
 patch fzf.vim fzf.vim.patch
 
