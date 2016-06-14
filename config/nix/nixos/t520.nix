14bc14b3c4277fcd1e4d380bdc86e3d80a81624e
Nick Spinale <spinalen@carleton.edu>
2016-06-14 01:05:11 -0500

    working wifi

--- config/nix/nixos/t520.nix
+++ config/nix/nixos/t520.nix
@@ -15,8 +15,21 @@
   boot.loader.gummiboot.enable = true;
 
   # networking.wicd.enable = true;
-  networking.wpa_supplicant.enable = true;
+  # networking.wpa_supplicant.enable = true;
   # networking.wireless.enable = true;
+  # networking.wireless.userControlled.enable = true;
 
-  # networking.networkmanager.enable = true;
+  # # networking.networkmanager.enable = true;
+  # networking = {
+  #   # interfaceMonitor.enable = false;
+  #   wireless.enable = false; # Don't run wpa_supplicant (wicd will do it when necessary)
+  #   useDHCP = false; # Don't run dhclient on wlan0
+  #   wicd.enable = true;
+  # };
+
+  networking = {
+    wireless.enable = false;
+    wireless.userControlled = true;
+  };
+
 }
