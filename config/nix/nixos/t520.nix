64c6f7d0e50fee0641189a6b713331556089c9dd
Nick Spinale <spinalen@carleton.edu>
2016-06-14 17:48:05 -0500

    add wpa conf from man page

--- config/nix/nixos/t520.nix
+++ config/nix/nixos/t520.nix
@@ -9,27 +9,18 @@
 
   environment.systemPackages = pkgs.mylib.gatherLists pkgs [
     ../lists/core.nix
+    # ../lists/huge.nix
+    # ../lists/current.nix
   ];
  
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.gummiboot.enable = true;
 
-  # networking.wicd.enable = true;
-  # networking.wpa_supplicant.enable = true;
-  # networking.wireless.enable = true;
-  # networking.wireless.userControlled.enable = true;
-
-  # # networking.networkmanager.enable = true;
-  # networking = {
-  #   # interfaceMonitor.enable = false;
-  #   wireless.enable = false; # Don't run wpa_supplicant (wicd will do it when necessary)
-  #   useDHCP = false; # Don't run dhclient on wlan0
-  #   wicd.enable = true;
-  # };
-
   networking = {
     wireless.enable = false;
-    wireless.userControlled = true;
+    wireless.userControlled.enable = true;
   };
+
+  system.stateVersion = "16.03";
 
 }
