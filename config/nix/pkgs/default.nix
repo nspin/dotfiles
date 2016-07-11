01fab2a1a2a3bd66fa9897929fa216d9ca4aa467
Nick Spinale <spinalen@carleton.edu>
2016-07-10 20:14:59 -0400

    add apktool

--- config/nix/pkgs/default.nix
+++ config/nix/pkgs/default.nix
@@ -9,6 +9,9 @@ pkgs: with pkgs; {
   ycm = callPackage ./local/ycm {};
   wicd = callPackage ./local/wicd {};
   mitmproxy = callPackage ./local/mitmproxy {};
+  apktool = callPackage ./local/apktool {
+    buildTools = androidenv.buildTools;
+  };
 
   opencvBloated = callPackage <nixpkgs/pkgs/development/libraries/opencv> {
     enableBloat = true;
