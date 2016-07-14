5cf4bc3de139ed3a940c03fb9d8af09d7d5f9446
Nick Spinale <spinalen@carleton.edu>
2016-07-14 01:12:43 -0400

    add haskell nix test bash binding

--- config/sh/current.sh
+++ config/sh/current.sh
@@ -1,3 +1,7 @@
 function nixtest () {
     nix-build -E "(import <nixpkgs> {}).callPackage ./$1 {}"
 }
+
+function nixhest () {
+    nix-build -E "(import <nixpkgs> {}).haskellPackages.callPackage ./$1 {}"
+}
