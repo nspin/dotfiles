d4ff42262283eab252fa70c84332eb617cae5a24
Nick Spinale <spinalen@carleton.edu>
2016-08-30 19:37:15 +0200

    more organization

--- config/nix/nixos/parts/graphical.nix
+++ config/nix/nixos/parts/graphical.nix
@@ -32,20 +32,21 @@
         '';
       };
 
-    displayManager = {
-      sessionCommands = ''
-        xrdb -merge ${<dotfig/nix/nixos/x11/xresources>}
-        xmodmap ${<dotfig/nix/nixos/x11/Xmodmap>}
+    displayManager.sessionCommands = ''
+        xrdb -merge ${./x11/xresources}
+        xmodmap ${./x11/Xmodmap}
         xset r rate 300 50
         feh --bg-max ~/.background.jpg
         # for img in ~./background.{png,jpg}; do
         #     feh --bg-max "$img"
         # done
         # xsetroot -solid '#002B36'
-      '';
-      slim.enable = true;
-      slim.defaultUser = "nick";
-      slim.theme = pkgs.fetchurl {
+    '';
+
+    displayManager.slim = {
+      enable = true;
+      defaultUser = "nick";
+      theme = pkgs.fetchurl {
           url = "https://github.com/jagajaga/nixos-slim-theme/archive/Final.tar.gz";
           sha256 = "4cab5987a7f1ad3cc463780d9f1ee3fbf43603105e6a6e538e4c2147bde3ee6b";
       };
