1d9a6281447e558f3cfb017045ce184c1187c7a0
Nick Spinale <spinalen@carleton.edu>
2016-08-30 19:54:27 +0200

    working on multi-user x

--- config/nix/nixos/parts/graphical.nix
+++ config/nix/nixos/parts/graphical.nix
@@ -19,18 +19,14 @@
     enable = true;
     layout = "us";
 
-    windowManager.wmdev =
-      let my-xmonad = pkgs.haskellPackages.callPackage <dotfig/xmonad> {};
-      in {
-        enable = true;
-        start = ''
-          # logdir=/var/log/wmdev
-          # mkdir -p $logdir
-          # ${my-xmonad}/bin/xmonad > $logdir/out.log 2> $logdir/err.log &
-          ${my-xmonad}/bin/xmonad
-          waitPID=$!
-        '';
+    displayManager.slim = {
+      enable = true;
+      # defaultUser = "nick";
+      theme = pkgs.fetchurl {
+          url = "https://github.com/jagajaga/nixos-slim-theme/archive/Final.tar.gz";
+          sha256 = "4cab5987a7f1ad3cc463780d9f1ee3fbf43603105e6a6e538e4c2147bde3ee6b";
       };
+    };
 
     displayManager.sessionCommands = ''
         xrdb -merge ${./x11/xresources}
@@ -43,14 +39,18 @@
         # xsetroot -solid '#002B36'
     '';
 
-    displayManager.slim = {
-      enable = true;
-      defaultUser = "nick";
-      theme = pkgs.fetchurl {
-          url = "https://github.com/jagajaga/nixos-slim-theme/archive/Final.tar.gz";
-          sha256 = "4cab5987a7f1ad3cc463780d9f1ee3fbf43603105e6a6e538e4c2147bde3ee6b";
+    windowManager.wmdev =
+      let my-xmonad = pkgs.haskellPackages.callPackage <dotfig/xmonad> {};
+      in {
+        enable = true;
+        start = ''
+          # logdir=/var/log/wmdev
+          # mkdir -p $logdir
+          # ${my-xmonad}/bin/xmonad > $logdir/out.log 2> $logdir/err.log &
+          ${my-xmonad}/bin/xmonad
+          waitPID=$!
+        '';
       };
-    };
 
     desktopManager.xterm.enable = false;
 
