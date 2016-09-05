c8636463ac80841b263962a13eb7735f0a0f1ba0
Nick Spinale <spinalen@carleton.edu>
2016-09-05 18:44:30 +0200

    working on moz

--- config/firefox/user.js
+++ config/firefox/user.js
@@ -3,3 +3,4 @@ user_pref("browser.search.defaultenginename.US", "Google");
 user_pref("browser.fullscreen.animate", false);
 user_pref("browser.panorama.animate_zoom", false);
 user_pref("browser.tabs.animate", false);
+user_pref("network.protocol-handler.external.magnet", true);
