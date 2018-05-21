37e834d04d4ac8166aa7cd209dab43840d8328a1
Nick Spinale <nick@nickspinale.com>
2018-05-20 21:31:02 -0500

    headless

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 tmux:
     xclip binding for vm
-    cwd
 
 --------------
 
--- config/xdg/firefox/user.js
+++ config/xdg/firefox/user.js
@@ -1,6 +1,6 @@
-user_pref("browser.search.defaultenginename", "Google");
-user_pref("browser.search.defaultenginename.US", "Google");
+user_pref("browser.tabs.animate", false);
 user_pref("browser.fullscreen.animate", false);
 user_pref("browser.panorama.animate_zoom", false);
-user_pref("browser.tabs.animate", false);
 user_pref("network.protocol-handler.external.magnet", true);
+user_pref("browser.search.defaultenginename", "Google");
+user_pref("browser.search.defaultenginename.US", "Google");
