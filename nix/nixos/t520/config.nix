a8a1dcea9933585fb4cded4d89240ac5fac6fcfc
Nick Spinale <nick@nickspinale.com>
2018-06-25 06:47:11 -0500

    Update t520 nix env

--- nix/nixos/t520/config.nix
+++ nix/nixos/t520/config.nix
@@ -4,34 +4,21 @@
 
   nix.nixPath = [
     "nixpkgs=/cfg/nixpkgs"
-    "nixos-config=/etc/nixos/configuration.nix"
     "dotfiles=/cfg/dotfiles"
     "local=/cfg/local"
   ];
 
-  security.sudo.extraConfig = ''
-    Defaults env_keep += "EDITOR"
-    Defaults env_keep += "VISUAL"
-    Defaults env_keep += "TERMINAL"
-
-    Defaults env_keep += "FZF_DEFAULT_OPTS"
-
-    Defaults env_keep += "MY_SYSTEM"
-    Defaults env_keep += "MY_DOTFILES"
-    Defaults env_keep += "MY_LOCAL"
-    Defaults env_keep += "MY_VIM_BUNDLE"
-    Defaults env_keep += "MY_VIM_PLUGIN_PATH"
-
-    Defaults env_keep += "NIX_PATH"
-  '';
-
   environment.variables = rec {
+    PAGER = "less -R";
     EDITOR = "vim";
     VISUAL = "vim";
     TERMINAL = "urxvt";
     BROWSER = "chromium";
 
     FZF_DEFAULT_OPTS = "--reverse";
+
+    NIXPKGS_CONFIG = MY_DOTFILES + "/nix/pkgs/config.nix";
+    NIXOS_CONFIG = MY_LOCAL + "/config.nix";
 
     MY_SYSTEM = "linux";
     MY_DOTFILES = "/cfg/dotfiles";
@@ -41,8 +28,28 @@
   };
 
   environment.extraInit = ''
-    export PATH="$MY_DOTFILES/bin/linux:$MY_DOTFILES/bin:$MY_LOCAL/bin:$PATH"
+    export PATH="$MY_LOCAL/bin:$(find $MY_DOTFILES/bin/linux -type d -printf ":%p"):$MY_DOTFILES/bin:$PATH"
     export XDG_DATA_DIRS="/cfg/local/share:$XDG_DATA_DIRS"
+  '';
+
+  security.sudo.extraConfig = ''
+    Defaults env_keep += "NIX_PATH"
+
+    Defaults env_keep += "PAGER"
+    Defaults env_keep += "EDITOR"
+    Defaults env_keep += "VISUAL"
+    Defaults env_keep += "TERMINAL"
+
+    Defaults env_keep += "FZF_DEFAULT_OPTS"
+
+    Defaults env_keep += "NIXPKGS_CONFIG"
+    Defaults env_keep += "NIXOS_CONFIG"
+
+    Defaults env_keep += "MY_SYSTEM"
+    Defaults env_keep += "MY_DOTFILES"
+    Defaults env_keep += "MY_LOCAL"
+    Defaults env_keep += "MY_VIM_BUNDLE"
+    Defaults env_keep += "MY_VIM_PLUGIN_PATH"
   '';
 
 }
