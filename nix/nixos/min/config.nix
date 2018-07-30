b60ca05a7820d21344d80cc116864a040215f83c
Nick Spinale <nick@nickspinale.com>
2018-07-30 06:44:04 +0000

    variable user space config root

--- nix/nixos/min/config.nix
+++ nix/nixos/min/config.nix
@@ -1,55 +1,77 @@
-{ pkgs, ... }: {
+{ config, lib, pkgs, ... }:
 
-  nixpkgs.config = import <dotfiles/nix/pkgs/config.nix>;
+with lib;
 
-  nix.nixPath = [
-    "nixpkgs=/cfg/nixpkgs"
-    "dotfiles=/cfg/dotfiles"
-    "local=/cfg/local"
-  ];
+let
 
-  environment.variables = rec {
-    PAGER = "less -R";
-    EDITOR = "vim";
-    VISUAL = "vim";
-    TERMINAL = "urxvt";
-    BROWSER = "chromium";
+  cfg = config.my.config;
+  prefix = cfg.prefix;
 
-    FZF_DEFAULT_OPTS = "--reverse";
+in {
 
-    NIXPKGS_CONFIG = MY_DOTFILES + "/nix/pkgs/config.nix";
-    NIXOS_CONFIG = MY_LOCAL + "/config.nix";
+  options = {
 
-    MY_SYSTEM = "linux";
-    MY_DOTFILES = "/cfg/dotfiles";
-    MY_LOCAL = "/cfg/local";
-    MY_VIM_BUNDLE = "/cfg/vim-bundle/bundle";
-    MY_VIM_PLUGIN_PATH = "${pkgs.vimPlugins.youcompleteme}/share/vim-plugins/youcompleteme";
+    my.config.prefix = mkOption {
+      type = types.str;
+      default = "/cfg/";
+    };
+
   };
 
-  environment.extraInit = ''
-    export PATH="$MY_LOCAL/bin:$(find $MY_DOTFILES/bin/linux -type d -printf ":%p"):$MY_DOTFILES/bin:$PATH"
-    export XDG_DATA_DIRS="/cfg/local/share:$XDG_DATA_DIRS"
-  '';
+  config = {
 
-  security.sudo.extraConfig = ''
-    Defaults env_keep += "NIX_PATH"
+    nixpkgs.config = import <dotfiles/nix/pkgs/config.nix>;
 
-    Defaults env_keep += "PAGER"
-    Defaults env_keep += "EDITOR"
-    Defaults env_keep += "VISUAL"
-    Defaults env_keep += "TERMINAL"
+    nix.nixPath = [
+      "nixpkgs=${prefix}nixpkgs"
+      "dotfiles=${prefix}dotfiles"
+      "local=${prefix}local"
+    ];
 
-    Defaults env_keep += "FZF_DEFAULT_OPTS"
+    environment.variables = rec {
+      PAGER = "less -R";
+      EDITOR = "vim";
+      VISUAL = "vim";
+      TERMINAL = "urxvt";
+      BROWSER = "chromium";
 
-    Defaults env_keep += "NIXPKGS_CONFIG"
-    Defaults env_keep += "NIXOS_CONFIG"
+      FZF_DEFAULT_OPTS = "--reverse";
 
-    Defaults env_keep += "MY_SYSTEM"
-    Defaults env_keep += "MY_DOTFILES"
-    Defaults env_keep += "MY_LOCAL"
-    Defaults env_keep += "MY_VIM_BUNDLE"
-    Defaults env_keep += "MY_VIM_PLUGIN_PATH"
-  '';
+      NIXPKGS_CONFIG = MY_DOTFILES + "/nix/pkgs/config.nix";
+      NIXOS_CONFIG = MY_LOCAL + "/config.nix";
+
+      MY_SYSTEM = "linux";
+      MY_DOTFILES = "${prefix}dotfiles";
+      MY_LOCAL = "${prefix}local";
+      MY_VIM_BUNDLE = "${prefix}vim-bundle/bundle";
+      MY_VIM_PLUGIN_PATH = "${pkgs.vimPlugins.youcompleteme}/share/vim-plugins/youcompleteme";
+    };
+
+    environment.extraInit = ''
+      export PATH="$MY_LOCAL/bin:$(find $MY_DOTFILES/bin/linux -type d -printf ":%p"):$MY_DOTFILES/bin:$PATH"
+      export XDG_DATA_DIRS="${prefix}local/share:$XDG_DATA_DIRS"
+    '';
+
+    security.sudo.extraConfig = ''
+      Defaults env_keep += "NIX_PATH"
+
+      Defaults env_keep += "PAGER"
+      Defaults env_keep += "EDITOR"
+      Defaults env_keep += "VISUAL"
+      Defaults env_keep += "TERMINAL"
+
+      Defaults env_keep += "FZF_DEFAULT_OPTS"
+
+      Defaults env_keep += "NIXPKGS_CONFIG"
+      Defaults env_keep += "NIXOS_CONFIG"
+
+      Defaults env_keep += "MY_SYSTEM"
+      Defaults env_keep += "MY_DOTFILES"
+      Defaults env_keep += "MY_LOCAL"
+      Defaults env_keep += "MY_VIM_BUNDLE"
+      Defaults env_keep += "MY_VIM_PLUGIN_PATH"
+    '';
+
+  };
 
 }
