5cbcd042b9b45cf394e679ea6ad2f0cffc93c1e3
Nick Spinale <nick@nickspinale.com>
2018-09-01 19:20:37 +0000

    vbox dummy keys

--- nix/nixos/min/vbox/default.nix
+++ nix/nixos/min/vbox/default.nix
@@ -1,15 +1,18 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
 
   imports = [
-    ./config.nix
-    ./pkgs.nix
+    ../config.nix
+    ../pkgs.nix
   ];
 
   networking.firewall.enable = false;
 
   services.openssh.enable = true;
+  services.openssh.hostKeys = [
+    { type = "ecdsa"; bits = "256"; path = "${config.my.config.dotfiles}/nix/nixos/min/vbox/id_ecdsa"; }
+  ];
 
   security.sudo.wheelNeedsPassword = false;
 
@@ -17,8 +20,11 @@
   users.extraUsers.x = {
     uid = 1000;
     isNormalUser = true;
-    password = "";
     extraGroups = [ "wheel" "docker" "vboxsf" ];
+    password = "";
+    openssh.authorizedKeys.keyFiles = [
+      ./id_ecdsa.pub
+    ];
   };
 
   virtualisation.virtualbox.guest.enable = true;
