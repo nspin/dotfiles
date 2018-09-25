9b80f2ddce7adc524f3e46a469fcf0422a6d4348
Nick Spinale <nick@nickspinale.com>
2018-09-25 10:26:34 -0400

    yes

--- nix/pkgs/darwin-env.nix
+++ nix/pkgs/darwin-env.nix
@@ -109,4 +109,20 @@ pkgs: with pkgs; [
 
   # gnupg
 
+  (dotfiles {
+    target = "/Users/nick/dotfiles/config";
+    assocs = {
+      ".bash_profile"          = "bash/bash_profile";
+      ".bashrc"                = "bash/bashrc";
+      ".tmux.conf"             = "multiplexers/tmux.conf";
+      ".vimrc"                 = "vim/vimrc";
+      ".config/git/config"     = "git/config";
+      ".config/git/ignore"     = "git/ignore";
+      ".ghci"                  = "interpreters/ghci";
+      ".editrc"                = "line-editors/editrc";
+      ".haskeline"             = "line-editors/haskeline";
+      ".inputrc"               = "line-editors/inputrc";
+    };
+  })
+
 ] ++ vim-plugins.all
