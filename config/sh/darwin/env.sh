a0280c543e609a44ef1fff71f5a8e9a046f425d3
Nick Spinale <nick@nickspinale.com>
2018-09-26 06:18:33 -0400

    fix darwin

--- config/sh/darwin/env.sh
+++ config/sh/darwin/env.sh
@@ -1,21 +1,16 @@
 export MY_SYSTEM="darwin"
 export MY_DOTFILES="$HOME/dotfiles"
 export MY_LOCAL="$HOME/local"
-export MY_VIM_BUNDLE="$HOME/vim-bundle/bundle"
 
-if [ -e $MY_LOCAL/vim_plugin_path ]; then
-    export MY_VIM_PLUGIN_PATH="$(cat $MY_LOCAL/vim_plugin_path | tr '\n' ':')"
-fi
+export NIX_PATH="nixpkgs=$HOME/nixpkgs:dotfiles=$MY_DOTFILES:local=$MY_LOCAL"
+export NIX_PROFILES="/nix/var/nix/profiles/per-user/$USER/profile /nix/var/nix/profiles/default"
 
-export PATH="$MY_LOCAL/bin:$MY_DOTFILES/bin/darwin:$MY_DOTFILES/bin:$PATH"
+export PATH="$MY_LOCAL/bin:$PATH"
+export PATH="$MY_DOTFILES/bin/darwin:$MY_DOTFILES/bin:$PATH"
 export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+export PATH="/opt/local/bin:$PATH"
 
 export EDITOR=vim
 export VISUAL=vim
 
 export FZF_DEFAULT_OPTS='--reverse'
-
-export NIX_PATH="nixpkgs=$HOME/nixpkgs:dotfiles=$MY_DOTFILES:local=$MY_LOCAL"
-
-export NIX_PROFILES="/nix/var/nix/profiles/per-user/$USER/profile /nix/var/nix/profiles/default"
