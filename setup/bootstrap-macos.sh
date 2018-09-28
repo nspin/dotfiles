93da2ee45875468b34bb0102e0f92cbaeedc2808
Nick Spinale <nick@nickspinale.com>
2018-09-28 18:19:40 -0400

    fix macos bootstrap

--- setup/bootstrap-macos.sh
+++ setup/bootstrap-macos.sh
@@ -7,9 +7,10 @@ fi
 export MY_NIXPKGS=$HOME/nixpkgs
 export MY_DOTFILES=$HOME/dotfiles
 export MY_LOCAL=$HOME/local
+export MY_PRIVATE=$HOME/private
 
 export NIXPKGS_CONFIG="$MY_DOTFILES/nix/pkgs/config.nix"
-export NIX_PATH="nixpkgs=$MY_NIXPKGS:dotfiles=$MY_DOTFILES:local=$MY_LOCAL"
+export NIX_PATH="nixpkgs=$MY_NIXPKGS:dotfiles=$MY_DOTFILES:local=$MY_LOCAL:private=$MY_PRIVATE:nixpkgs-overlays=$MY_DOTFILES/nix/pkgs/overlays.nix"
 
 nix-env -f '<nixpkgs>' -iA darwin-env
 
