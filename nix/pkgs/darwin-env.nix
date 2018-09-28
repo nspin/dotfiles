a210a2643f79cacee76a240fcec1f833572e25b6
Nick Spinale <nick@nickspinale.com>
2018-09-28 10:21:06 -0400

    fix bugs and move bulk of user pkgs

--- nix/pkgs/darwin-env.nix
+++ nix/pkgs/darwin-env.nix
@@ -1,113 +1,22 @@
 pkgs: with pkgs; [
 
+  bashInteractive
+  coreutils
   gnused
   gnugrep
   findutils
-  bashInteractive
-
-  reattach-to-user-namespace
-
-  # nix-repl
-  nix-prefetch-scripts
-  patchelf
-
-  rlwrap
-
-  coreutils
-  gnumake
   which
-  zip
-  unzip
-  rsync
   file
 
   my-vim
   tmux
   git
-  mercurial
-  subversion
   fzf
+  reattach-to-user-namespace
 
-  openssl
-
-  # jre jdk
-
-  python27
-
-  (python3.buildEnv.override {
-    extraLibs = with python3Packages; [
-      cffi
-      requests
-      beautifulsoup4
-      lxml
-      # numpy
-      # scipy
-      # sympy
-
-      pygments
-
-      # pillow
-
-      # notebook
-      # jupyter_console
-      # nbconvert
-      # ipykernel
-      # ipywidgets
-    ];
-  })
-
-  # cabal2nix
-  # haskellPackages.cabal-install
-  # haskellPackages.alex
-  # haskellPackages.happy
-  # haskellPackages.pandoc
-  (haskellPackages.ghcWithPackages (hp: with hp; [
-    # random
-    # bytestring
-    # stm
-    # vector
-    # containers
-    # mtl
-    # network
-    # async
-    # attoparsec
-    # unix
-    # process
-  ]))
-
-  (texlive.combine {
-    inherit (texlive)
-      # luatex
-      scheme-small
-      collection-latexextra
-      collection-fontsrecommended
-      fontawesome
-      physics
-    ;
-  })
-
-  readme-preview
-
-  nodejs
-  # clojure
-  # leiningen
-  # go
-  # gcc
-
-  cmake
-  # maven
-
-  # nmap
-  wget
-  # gdb
-  # nasm
-  # radare2
-  # mitmproxy
-  # qemu
-
-  # (lowPrio ihaskell)
-
-  # gnupg
+  nix-prefetch-scripts
+  patchelf
+  rlwrap
 
   (dotfiles {
     target = "/Users/nick/dotfiles/config";
