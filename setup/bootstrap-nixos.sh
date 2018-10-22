13f2dc9b9ca0def3c3e3800a0e2814a13567e820
Nick Spinale <nick@nickspinale.com>
2018-10-22 15:24:08 +0100

    boostrap nixos

--- setup/bootstrap-nixos.sh
+++ setup/bootstrap-nixos.sh
@@ -1,8 +1,6 @@
 export MY_DOTFILES=/cfg/dotfiles
 export MY_LOCAL=/cfg/local
 export MY_NIXPKGS=/cfg/nixpkgs
-export NIXPKGS_CONFIG="$MY_DOTFILES/nix/pkgs/config.nix"
-export NIXOS_CONFIG="$MY_LOCAL/config.nix"
-export NIX_PATH="nixpkgs=$MY_NIXPKGS:dotfiles=$MY_DOTFILES:local=$MY_LOCAL:private=$MY_PRIVATE:nixpkgs-overlays=$MY_DOTFILES/nix/pkgs/overlays.nix"
+export NIX_PATH="nixpkgs=$MY_NIXPKGS:dotfiles=$MY_DOTFILES:local=$MY_LOCAL:private=$MY_PRIVATE:nixos-config=$MY_LOCAL/config.nix"
 
-nixos-rebuild switch
+nixos-rebuild switch && update-dotfile-links
