be4b970215e7567e924cfdcd8f4d22cb9bd9e215
Nick Spinale <nick@nickspinale.com>
2018-10-22 16:41:38 +0000

    nixos bootstrap

--- setup/bootstrap-nixos.sh
+++ setup/bootstrap-nixos.sh
@@ -1,6 +1,8 @@
 export MY_DOTFILES=/cfg/dotfiles
 export MY_LOCAL=/cfg/local
 export MY_NIXPKGS=/cfg/nixpkgs
-export NIX_PATH="nixpkgs=$MY_NIXPKGS:dotfiles=$MY_DOTFILES:local=$MY_LOCAL:private=$MY_PRIVATE:nixos-config=$MY_LOCAL/config.nix"
+export NIX_PATH="nixpkgs=$MY_NIXPKGS:dotfiles=$MY_DOTFILES:local=$MY_LOCAL:private=$MY_PRIVATE:nixos-config=$MY_DOTFILES/nix/nixos/config.nix"
 
-nixos-rebuild switch && update-dotfile-links
+nixos-rebuild switch
+
+# update-dotfile-links
