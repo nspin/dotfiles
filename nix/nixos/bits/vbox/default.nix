8928914b0857d7112126e78b8c98ecd45f95d8fe
Nick Spinale <nick@nickspinale.com>
2018-11-02 23:02:17 +0000

    expand vbox

--- nix/nixos/bits/vbox/default.nix
+++ nix/nixos/bits/vbox/default.nix
@@ -10,9 +10,18 @@
 
   boot.growPartition = true;
   boot.loader.grub.device = "/dev/sda";
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     autoResize = true;
   };
+
+  swapDevices = [{
+    device = "/dev/disk/by-label/swap";
+  }];
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
 
 }
