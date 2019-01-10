25b9f43eff329f745c53852853f567c2975ee310
Nick Spinale <nick@nickspinale.com>
2019-01-10 12:57:46 +0000

    bump grip version

--- nix/pkgs/overlay/pkgs/grip/default.nix
+++ nix/pkgs/overlay/pkgs/grip/default.nix
@@ -1,10 +1,12 @@
 { fetchzip, python27Packages }: 
-with python27Packages; buildPythonPackage {
-  name = "grip-4.3.2";
+with python27Packages; buildPythonPackage rec {
+  pname = "grip";
+  version = "4.5.2";
+  name = "${pname}-${version}";
 
-  src = fetchzip {
-    url = "https://pypi.python.org/packages/8b/9d/698a7a3a8b57c28eacac27f269c9d0da228d20ee734edbe9451e3e0f7cde/grip-4.3.2.zip";
-    sha256 = "08d1n640r5f25ys4w6ifcirp6hqw50g1vmblxf2lhsbr9zili3dk";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "03xacz7w2ga7y902n4v7xf3idcvyy900ilav6kp0pn4ffdf7x2q4";
   };
 
   propagatedBuildInputs = [
