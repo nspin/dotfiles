a27518b4ddc6bfeb3a1d23731e5b5fcad7afbc37
Nick Spinale <nick@nickspinale.com>
2019-04-16 21:28:18 +0000

    snrs link profile

--- config/sh/nix.sh
+++ config/sh/nix.sh
@@ -8,11 +8,15 @@ alias cabaldef='cabal2nix . > default.nix'
 alias cabalsh='cp ~/dotfiles/store/shell.nix .'
 
 snrs() {
+    profile=/nix/var/nix/profiles/system
     cfg=$(nixosbuild --no-out-link)
     if [ $? != 0 ]; then
         return $?
     fi
-    echo "switching to: $cfg"
+    echo "TOPLEVEL: $cfg"
+    echo "linking profile"
+    sudo nix-env -p $profile --set $cfg
+    echo "switching to configuration"
     sudo $cfg/bin/switch-to-configuration switch
 }
 
