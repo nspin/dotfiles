f59158d523843ab3e901c31591408204d8733808
Nick Spinale <nick@nickspinale.com>
2019-04-17 09:18:08 +0000

    clean up nixos boot

--- setup/bootstrap-nixos.sh
+++ setup/bootstrap-nixos.sh
@@ -2,16 +2,20 @@ set -e
 
 . ./vars-nixos.sh
 
-profile=/nix/var/nix/profiles/system
-cfg=$(nix-build '<top>' -A build.toplevel --no-out-link)
-if [ $? != 0 ]; then
-    return $?
-fi
-echo "TOPLEVEL: $cfg"
-echo "linking profile"
-sudo nix-env -p $profile --set $cfg
-echo "switching to configuration"
-sudo $cfg/bin/switch-to-configuration switch
+snrs() {
+    profile=/nix/var/nix/profiles/system
+    cfg=$(nixosbuild --no-out-link)
+    if [ $? != 0 ]; then
+        return $?
+    fi
+    echo "TOPLEVEL: $cfg"
+    echo "linking profile"
+    sudo nix-env -p $profile --set $cfg
+    echo "switching to configuration"
+    sudo $cfg/bin/switch-to-configuration switch
+}
+
+snrs
 
 export NIX_PROFILES="$HOME/.nix-profile /nix/var/nix/profiles/default /run/current-system/sw"
 
