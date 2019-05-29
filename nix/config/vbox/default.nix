b5937930951d9e4594c5ad62e14e5eb1744473d5
Nick Spinale <nick@nickspinale.com>
2019-05-29 12:19:58 +0000

    rm vbox details

--- nix/config/vbox/default.nix
+++ nix/config/vbox/default.nix
@@ -8,15 +8,6 @@
 
   virtualisation.virtualbox.guest.enable = true;
 
-  boot.growPartition = true;
-  boot.loader.grub.device = "/dev/sda";
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-    autoResize = true;
-  };
-
   boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
 
 }
