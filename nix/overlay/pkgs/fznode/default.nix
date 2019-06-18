88327ee7fbb4eeb9051fea491b1edd76525c01d4
Nick Spinale <nick@nickspinale.com>
2019-06-18 19:50:13 +0100

    fix fznode nix

--- nix/overlay/pkgs/fznode/default.nix
+++ nix/overlay/pkgs/fznode/default.nix
@@ -4,6 +4,7 @@ python3Packages.buildPythonPackage {
   name = "fznode";
   src = fetchgit {
     url = "https://github.com/nspin/fznode.git";
-    sha256 = "1zfkrir3qrnni69m3y2asj28jpfsdr46hap57ixfrca4zpzrwjjj";
+    rev = "bd8941a9c151f6a01acb65a01239095f3aacc2ee";
+    sha256 = "0444rwbx8j1sxw359x7x1dllaigjcgb0nkffzjqr7875g2mn1sr7";
   };
 }
